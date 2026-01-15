0f9a09e040d40c2734f1337bb0121f83b7bcdb09
Benoit Maurin <maurinbe@gmail.com>
2026-01-15 20:33:42 +0100

    solve temp http issue

--- frontend/src/app/GN2CommonModule/form/advanced-section/advanced-section.component.ts
+++ frontend/src/app/GN2CommonModule/form/advanced-section/advanced-section.component.ts
@@ -6,6 +6,19 @@ export enum AdvancedSectionState {
   EXPANDED = 'expanded',
 }
 
+export function uuid4() {
+    // from ipython project
+    // http://www.ietf.org/rfc/rfc4122.txt
+    const s = new Array(32);
+    const hex_digits = "0123456789ABCDEF";
+    for (let i = 0; i < 32; i++) {
+        s[i] = hex_digits[Math.floor(Math.random() * 0x10)];
+    }
+    s[12] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+    s[16] = hex_digits[(s[16].charCodeAt(0) & 0x3) | 0x8]; // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    return s.join("");
+}
+
 @Component({
   selector: 'gn-advanced-section',
   templateUrl: 'advanced-section.component.html',
@@ -36,7 +49,7 @@ export class AdvancedSectionComponent implements AfterViewChecked {
   state: AdvancedSectionState = AdvancedSectionState.COLLAPSED;
 
   hideParent: boolean = false;
-  advancedPanelID: string = 'advancedSection-' + window.crypto.randomUUID().slice(0, 5);
+  advancedPanelID: string = 'advancedSection-' + uuid4().slice(0, 5);
 
   ngAfterViewChecked() {
     // Hide the Advanced button if no forms (or anything else) in the advanced section
--- frontend/src/webpack.config.js
+++ frontend/src/webpack.config.js
@@ -1,6 +1,12 @@
 const webpack = require('webpack');
 
 module.exports = {
+  devServer : {
+ allowedHosts: [
+  'all',
+   ],
+      disableHostCheck: true
+  },
   plugins: [
     new webpack.ProvidePlugin({
       process: 'process/browser',
